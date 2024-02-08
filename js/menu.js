6b2dcb906fe5291d7656e0831e8f42008b9dee2a
Gestoria Integral Hodah <gestoria.hbquilmes@gmail.com>
2024-02-08 10:54:38 -0300

    Ups me equivoque
    Cambie algunos detalles que creí que podrian funcionar mejor

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@
      const openButton = document.querySelector('.nav__menu');
      const menu = document.querySelector('.nav__link');
      const closeMenu = document.querySelector('.nav__close');
+     const menuLinks = document.querySelector('.menu a[href^="#"]');
 
 
      openButton.addEventListener('click', ()=>{
@@ -14,6 +15,12 @@
 
      });
 
+      menuLinks.addEventListener('click', ()=>{
+         menu.classList.remove('nav__link--show');
+      })
+
+     })
+
      
 
-})();
+();
